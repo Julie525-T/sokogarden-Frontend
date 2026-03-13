ba7e7ce54cd17b366318566c21b429538ea6cd7a
Julie525-T <jullytracy576@gmail.com>
2026-03-13 13:33:09 +0300

    I have updated my sign in and sign up component

--- sokogarden/src/components/SignIn.jsx
+++ sokogarden/src/components/SignIn.jsx
@@ -1,9 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+
 
 const SignIn=()=>{
+
+    // Initializing hooks
+
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+
+    const[loading,setLoading]=useState("")
+    const[success,setSuccess]=useState("")
+    const[error,setError]=useState("")
+
+    const navigate = useNavigate();
+
+    // Creating a function to send our data to the database
+
+    const submit=async(e)=>{
+
+        e.preventDefault()
+        setLoading("Please wait as we check your data")
+
+        try {
+
+            const data=new FormData()
+
+            data.append("email",email)
+            data.append("password",password)
+
+            const response=await axios.post("https://julie.alwaysdata.net/api/signin",data)
+
+            setLoading("")  // After successful posting,clear the loading message 
+
+            // Check if the response has user item,
+
+            if (response.data.user) {
+                // If user is found, store user details in localstorage
+
+                localStorage.setItem ("user", JSON.stringify(response.data.user));
+                setSuccess(response.data.message);
+
+                // Redirect to /get products component
+
+                setTimeout(()=>{
+                    navigate("/");
+                },2000)
+            }
+            
+            else{
+                // user not found, show error message
+
+                setError(response.data.message)
+            }
+
+
+            
+        } catch (error) {
+            
+        }
+
+    }
+
+
+   
+
+    
     return(
-        <div>
-            <h1>Welcome to Sign In</h1>
+        <div className="row justify-content-center">
+
+            <div className="col-md-6 card shadow">
+
+                <h1>Sign In</h1>
+
+                <form action="" onSubmit={submit}>
+
+                    <p className="text-warning">{loading}</p>
+                    <p className="text success">{success}</p>
+                    <p className="text-danger">{error}</p>
+
+                    <input type="text" placeholder="Enter your email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)}required/>
+                    <br />
+                    <br />
+                    <input type="text" placeholder="Enter your password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                    <br />
+                    <br />
+                    <input type="submit" value="Sign In" className="btn btn-success text-white w-100"/>
+
+                    <p>Don't have an account? <Link to="/signup" >Sign Up</Link></p> 
+                    <br />
+                    <br />
+                </form>
+
+            </div>
+            
         </div>
     )
 }
--- sokogarden/src/components/SignUp.jsx
+++ sokogarden/src/components/SignUp.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -5,10 +6,55 @@ const SignUp=()=>{
 
     // Initializing our hooks
 
-    const [username,setUsername]=useState()
-    const[email,setEmail]=useState()
-    const[phone,setPhone]=useState()
-    const[password,setPassword]=useState()
+    const [username,setUsername]=useState("")
+    const[email,setEmail]=useState("")
+    const[phone,setPhone]=useState("")
+    const[password,setPassword]=useState("")
+
+    const[loading,setLoading]=useState("")
+    const[success,setSuccess]=useState("")
+    const[error,setError]=useState("")
+
+    // Create a function to send our data to the server
+
+    const submit=async(e)=>{
+
+        e.preventDefault()
+        setLoading("Please wait as we upload your data")
+        
+        try {
+
+            const data=new FormData()
+
+            data.append ("username",username)
+            data.append ("email",email)
+            data.append ("phone",phone)
+            data.append ("password",password)
+
+            // Call our API to send data to the database
+
+            const response=await axios.post("https://julie.alwaysdata.net/api/signup",data)
+
+            setLoading("")
+
+            setSuccess(response.data.message)
+
+            // Reset the form after sending data
+
+            setUsername("")
+            setEmail("")
+            setPhone("")
+            setPassword("")
+
+            
+        } catch (error) {
+
+            setLoading("")
+            setError(error.message)
+            
+        }
+
+    }
 
     return(
         <div className="row justify-content-center">
@@ -18,21 +64,25 @@ const SignUp=()=>{
                 <h1>Sign Up</h1>
 
 
-                <form action="">
+                <form action="" onSubmit={submit} >
 
-                    <input type="text" placeholder="Enter your username" className="form-control" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+                    <p className="text-warning">{loading}</p>
+                    <p className="text-success">{success}</p>
+                    <p className="text-danger">{error}</p>
+
+                    <input type="text" placeholder="Enter your username" className="form-control" value={username} onChange={(e)=>setUsername(e.target.value)} required/>
                     <br />
                     
-                    <input type="email" placeholder="Enter email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="email" placeholder="Enter email" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
                     <br />
                     
-                    <input type="tel" placeholder="Enter phone number" className="form-control" value={phone} onChange={(e)=>setPhone(e.target.value)}/>
+                    <input type="tel" placeholder="Enter phone number" className="form-control" value={phone} onChange={(e)=>setPhone(e.target.value)} required/>
                     <br />
                     
-                    <input type="text" placeholder="Enter your password" className="form-control"value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                    <input type="text" placeholder="Enter your password" className="form-control"value={password} onChange={(e)=>setPassword(e.target.value)} required/>
                     <br />
 
-                    <input type="submit" value="Sign Up" className="btn btn-primary text-white w-100"/>
+                    <input type="submit" value="Sign Up" className="btn btn-success text-white w-100"/>
                     <br />
                     <br />
                     <p>Already have an account? <Link to={"/signin"}>Sign In</Link></p>
